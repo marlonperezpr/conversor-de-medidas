668df1a326f53bee47aba2cb7760cb8b7c4fb588
marlonperezpr <marlonperezpr@gmail.com>
2024-08-28 15:32:13 -0300

    primeiro commit

---
+++ conversor.py
@@ -0,0 +1,19 @@
+#CONVERSOR DE MEDIDAS EM PYTHON
+
+def celsius_para_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_para_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+def metros_para_pes(metros):
+    return metros * 3.28084
+
+def pes_para_metros(pes):
+    return pes / 3.28084
+
+def quilogramas_para_libras(quilogramas):
+    return quilogramas * 2.20462
+
+def libras_para_quilogramas(libras):
+    return libras / 
